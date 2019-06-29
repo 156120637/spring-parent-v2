b5ba3dd1a12c3e3678ed251e19b9db735ac9afb2
tianjiaqin <156120637@qq.com>
2019-06-29 10:58:44 +0800

    fix depedency

--- spring-web/spring-manager/spring-manager-biz/src/main/java/com/dangdang/biz/service/impl/LoginServiceImpl.java
+++ spring-web/spring-manager/spring-manager-biz/src/main/java/com/dangdang/biz/service/impl/LoginServiceImpl.java
@@ -3,14 +3,13 @@ package com.dangdang.biz.service.impl;
 import com.dangdang.biz.dto.LoginForm;
 import com.dangdang.biz.dto.LoginResult;
 import com.dangdang.biz.service.LoginService;
-import com.dangdang.common.exception.BaseException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoginServiceImpl implements LoginService {
 
     @Override
-    public LoginResult login(LoginForm form) throws BaseException {
+    public LoginResult login(LoginForm form) {
         return null;
     }
 }
