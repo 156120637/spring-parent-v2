983a6e93f44b6f3741e3a131447f8c6123b76814
tianjiaqin <156120637@qq.com>
2019-07-19 22:46:15 +0800

    feature xxl-job

---
+++ spring-job/spring-job-xxl/src/main/java/com/dangdang/job/xxl/SompleJobService.java
@@ -0,0 +1,13 @@
+package com.dangdang.job.xxl;
+
+/**
+ * Create by tianjiaqin 2019-07-19
+ * <p>
+ * xxl 分布式任务调度 xxl-job  https://github.com/xuxueli/xxl-job
+ * <p>
+ * 这个跟当当开源的  elastic-job 是类似的，更多的区别待研究之后再写
+ */
+public class SompleJobService {
+
+
+}
